e88504132b78844c8bedd99329716a5210afc7c5
Charles Sherwood <kulan876321@yahoo.com>
2025-03-26 10:32:42 -0400

    Test

--- Chapter3Exercise17.cpp
+++ Chapter3Exercise17.cpp
@@ -12,34 +12,76 @@ key and the correct solution will be shown.
 #include <ctime>
 using namespace std;
 
-double Choice() 
-{
-	
+double Menu();
+double Addition();
+double Subtraction();
+double Multiplication();
+double Division();
 
-}
 
 int main()
 {
-	double A, B, C, U;
+	Menu();
+}
 
+
+double Menu()
+{
+	int choice;
+	do {
+		cout << "1-Addition" << endl;
+		cout << "2-Subtraction" << endl;
+		cout << "3-Multiplication" << endl;
+		cout << "4-Division" << endl;
+		cout << "5-EXIT" << endl;
+		cin >> choice;
+		if (choice > 5 || choice < 1)
+		{
+			cout << "Please Enter A Valid Choice";
+			cin >> choice;
+		}
+		switch (choice)
+		{
+		case 1:
+		{
+			Addition();
+		}
+		break;
+		case 2:
+		{
+			Subtraction();
+		}
+		break;
+		case 3:
+		{
+			Multiplication();
+		}
+		break;
+		case 4:
+		{
+			Division();
+		}
+		break;
+		}
+	} while (choice != 5);
+	return 0;
+
+}
+
+
+double Addition()
+{
+	double A, B, C, U;
 	unsigned seed = time(0);
 	srand(seed);
-	//Let the user know what The program does.
-	cout << "Hello, Welcome to Math Tutor" << endl;
-	cout << "I will be helping you learn addition today" << "\n\n";
-	cout << "I will display two random numbers, try and solve for the answer." << endl;
-
-	//Get two random numbers and add them together
 	A = 1 + rand() % 350;
 	B = 1 + rand() % 350;
 	C = A + B;
-
-	cout << "\n";
-
-
+	cout << endl;
 	cout << A << " + " << B << " = ";
 	cin >> U;
-	if (U > C) {
+	if (U > C)
+	{
 		cout << "Incorect" << endl;
 	}
 	else if (U < C) {
@@ -53,5 +95,98 @@ int main()
 	cout << "Check answer?" << endl;
 	system("pause");
 	cout << A << " + " << B << " = " << C << endl;
-	return 0;
+	return	main();
+}
+double Subtraction()
+{
+	double A, B, C, U;
+	unsigned seed = time(0);
+	srand(seed);
+	A = 1 + rand() % 350;
+	B = 1 + rand() % 350;
+	if (A < B)
+	{
+		C = B - A;
+		cout << B << " - " << A << " = ";
+	}
+	else
+	{C = A - B;
+	cout << endl;
+	cout << A << " - " << B << " = ";
+	}
+	cin >> U;
+	if (U > C)
+	{
+		cout << "Incorect" << endl;
+	}
+	else if (U < C) {
+		cout << "Incorrect" << endl;
+	}
+	else {
+		cout << "Correct" << endl;
+
+	}
+	//pause the system until they enter a key
+	cout << "Check answer?" << endl;
+	system("pause");
+	cout << A << " - " << B << " = " << C << endl;
+	return	main();
+}
+double Multiplication()
+{
+	double A, B, C, U;
+	unsigned seed = time(0);
+	srand(seed);
+	A = 1 + rand() % 350;
+	B = 1 + rand() % 350;
+	C = A * B;
+	cout << endl;
+	cout << A << " * " << B << " = ";
+	cin >> U;
+	if (U > C)
+	{
+		cout << "Incorect" << endl;
+	}
+	else if (U < C) {
+		cout << "Incorrect" << endl;
+	}
+	else {
+		cout << "Correct" << endl;
+
+	}
+	//pause the system until they enter a key
+	cout << "Check answer?" << endl;
+	system("pause");
+	cout << A << " * " << B << " = " << C << endl;
+	return	main();
+
+}
+double Division()
+{
+	double A, B, C, U;
+	unsigned seed = time(0);
+	srand(seed);
+	A = 1 + rand() % 350;
+	B = 1 + rand() % 350;
+	C = A / B;
+	cout << endl;
+	cout << A << " / " << B << " = ";
+	cin >> U;
+	if (U > C)
+	{
+		cout << "Incorect" << endl;
+	}
+	else if (U < C) {
+		cout << "Incorrect" << endl;
+	}
+	else {
+		cout << "Correct" << endl;
+
+	}
+	//pause the system until they enter a key
+	cout << "Check answer?" << endl;
+	system("pause");
+	cout << A << " / " << B << " = " << C << endl;
+	return	main();
+
 }
